e103c75d2f5b88415c8cdd11e1df93a3fb0d3cac
akashenfelter <akashenfelter@bigml.com>
2016-11-16 14:11:27 -0800

    Improved anomaly-benchmarking test and added user-defined dimension categories

--- anomaly-benchmarking/test/test.sh
+++ anomaly-benchmarking/test/test.sh
@@ -13,15 +13,15 @@ run_bigmler whizzml --package-dir ../ --output-dir ./.build
 run_bigmler --train https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data --no-model \
             --project "Whizzml examples tests" --output-dir cmd/pre_test
 
-# building the inputs for the test
+# building the inputs for the first test
 prefix='[["dataset-list", ["'
-suffix='"]]]'
+suffix='"]], ["replicates", 1]]'
 text=''
 cat cmd/pre_test/dataset | while read dataset
 do
 echo "$prefix$dataset$suffix" > "test_inputs.json"
 done
-log "Testing generate-datasets script -------------------------------"
+log "Testing generate-datasets script (one replicate)  -------------"
 # running the execution with the given inputs
 run_bigmler execute --scripts .build/generate-datasets/scripts --inputs test_inputs.json \
                     --output-dir cmd/results
@@ -31,9 +31,33 @@ declare regex="\"outputs\": \[\[\"generated-datasets\", "
 declare file_content=$( cat "${file}" )
 if [[ " $file_content " =~ $regex ]]
     then
-        log "generate-datasets OK"
+        log "one replicate OK"
     else
-        echo "generate-datasets KO:\n $file_content"
+        echo "one replicate KO:\n $file_content"
+        exit 1
+fi
+
+# building the inputs for the second test
+prefix='[["dataset-list", ["'
+suffix='"]], ["diff-map", {"easy": 0.1333333}], ["freq-list", [0.001]]]'
+text=''
+cat cmd/pre_test/dataset | while read dataset
+do
+echo "$prefix$dataset$suffix" > "test_inputs.json"
+done
+log "Testing generate-datasets script (ten replicates)  -------------"
+# running the execution with the given inputs
+run_bigmler execute --scripts .build/generate-datasets/scripts --inputs test_inputs.json \
+                    --output-dir cmd/results
+# check the outputs
+declare file="cmd/results/whizzml_results.json"
+declare regex="\"outputs\": \[\[\"generated-datasets\", "
+declare file_content=$( cat "${file}" )
+if [[ " $file_content " =~ $regex ]]
+    then
+        log "ten replicates OK"
+    else
+        echo "ten replicates KO:\n $file_content"
         exit 1
 fi
 # remove the created resources
