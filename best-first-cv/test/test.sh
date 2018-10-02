bc61456befba31ffdeaf741ad3dcf6261668ca5e
Mercè Martín Prats <merce@bigml.com>
2018-10-02 18:20:08 +0200

    Requested review changes and tests for best-first-cv and multi-label

---
+++ best-first-cv/test/test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source ../../test-utils.sh
+
+rm -f -R cmd_del
+rm -f -R cmd
+rm -f -R .build
+
+log "-------------------------------------------------------"
+log "Test for best-first-cv script"
+run_bigmler whizzml --package-dir ../ --output-dir ./.build
+# creating the resources needed to run the test
+run_bigmler --train s3://bigml-public/csv/diabetes.csv --no-model \
+            --project "Whizzml examples tests" --output-dir cmd/pre_test
+# building the inputs for the test
+prefix='[["dataset-id", "'
+suffix='"], ["n", 2], ["objective-id" ,"diabetes"], ["pre-selected", ["plasma glucose"]]]'
+text=''
+cat cmd/pre_test/dataset | while read datasets
+do
+echo "$prefix$datasets$suffix" > "test_inputs.json"
+done
+log "Testing best-first-cv script -------------------------------"
+# running the execution with the given inputs
+run_bigmler execute --scripts .build/scripts --inputs test_inputs.json \
+                    --output-dir cmd/results --verbosity 1
+# check the outputs
+declare file="cmd/results/whizzml_results.json"
+declare regex="\"selected-fields\": \[\"plasma glucose\", \"bmi\"\]"
+declare file_content=$( cat "${file}" )
+if [[ " $file_content " =~ $regex ]]
+    then
+        log "best-first-cv OK"
+    else
+        echo "best-first-cv KO:\n $file_content"
+        exit 1
+fi
+# remove the created resources
+run_bigmler delete --from-dir cmd --output-dir cmd_del
+run_bigmler delete --from-dir .build --output-dir cmd_del
+rm -f -R test_inputs.json cmd cmd_del
+rm -f -R .build .bigmler*
---
+++ multi-label/test/test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+source ../../test-utils.sh
+
+rm -f -R cmd_del
+rm -f -R cmd
+rm -f -R .build1
+rm -f -R .build2
+rm -f -R .build3
+
+log "-------------------------------------------------------"
+log "Test for multi-label script"
+run_bigmler whizzml --package-dir ../../items-to-features --output-dir ./.build1
+run_bigmler whizzml --package-dir ../create-item-models \
+                    --output-dir ./.build2
+run_bigmler whizzml --package-dir ../ml-batch-prediction \
+                    --output-dir ./.build3
+
+# creating the resources needed to run the test
+run_bigmler --train ./multilabel.csv --no-model \
+            --source-attributes source.json \
+            --project "Whizzml examples tests" --output-dir cmd/pre_test
+# building the inputs for the test
+prefix='[["dataset", "'
+suffix='"], ["objective", "class"], ["model-kind" ,"model"], ["model-parameters", {}]]'
+text=''
+cat cmd/pre_test/dataset | while read datasets
+do
+echo "$prefix$datasets$suffix" > "test_inputs.json"
+done
+log "Testing multi-label script -------------------------------"
+# running the execution to create models with the given inputs
+run_bigmler execute --scripts .build2/create-item-models/scripts \
+                    --inputs test_inputs.json \
+                    --output-dir cmd/results --verbosity 1
+prefix='[["execution", "'
+suffix='"], ["dataset", "'
+suffix2='"], ["clean-up?" , true]]'
+text=''
+cat cmd/results/execution | while read execution
+do
+cat cmd/pre_test/dataset | while read datasets
+do
+echo "$prefix$execution$suffix$datasets$suffix2" > "test_inputs.json"
+done
+done
+# running the execution to create models with the given inputs
+run_bigmler execute --scripts .build3/ml-batch-prediction/scripts \
+                    --inputs test_inputs.json \
+                    --output-dir cmd/results --verbosity 1
+# check the outputs
+declare file="cmd/results/whizzml_results.json"
+declare regex="\"outputs\": \[\[\"result\", \"dataset\/"
+declare file_content=$( cat "${file}" )
+if [[ " $file_content " =~ $regex ]]
+    then
+        log "multi-label OK"
+    else
+        echo "multi-label KO:\n $file_content"
+        exit 1
+fi
+# remove the created resources
+cat cmd/results/execution | while read execution
+do
+run_bigmler delete --id "$execution" --output-dir cmd_del
+done
+run_bigmler delete --from-dir .build1 --output-dir cmd_del
+run_bigmler delete --from-dir .build2 --output-dir cmd_del
+run_bigmler delete --from-dir .build3 --output-dir cmd_del
+rm -f -R test_inputs.json cmd cmd_del
+rm -f -R .build* .bigmler*
